dd3ff95096ffb1f69c609dccb9b171639e09abb7
Naman Jain <naman1205jain@gmail.com>
2023-09-13 22:01:42 -0700

    bash scripts

--- inference/scripts/llama_apps_few_shot.sh
+++ inference/scripts/llama_apps_few_shot.sh
@@ -1,5 +1,5 @@
 python main.py \
 --model codellama/CodeLlama-7b-hf --use_auth_token \
 --trust_remote_code --tasks appsfewshot-introductory-base --batch_size 10 --n_samples 50 \
---max_sequence_length 6154 --precision bf16 --temperature 0.1 \
+--max_sequence_length 6154 --precision bf16 --temperature 0.1 --max_num_batched_tokens 16000 \
 --num_gpus 4 --exp_name clfs_intro_base_01 --allow_code_execution --shuffle --num_gpus 4 \
--- training_scripts/run_contests_llama13b_a100_80.sh
+++ training_scripts/run_contests_llama13b_a100_80.sh
@@ -1,13 +1,31 @@
+refactored_style=$1
+final_style=$2
+
+dictionary = {
+    'base_original': 'base',
+    'rename_original': 'rename',
+    'modularize_original': 'mod',
+    'remodularize_merged': 'remod',
+    'plan_merged1': 'planm1',
+    'plan_merged2': 'planm2',
+    'plan_merged1padall': 'planm1padall',
+    'plan_merged2padall': 'planm2padall',
+}
+
+short_refactored_style=${dictionary[$refactored_style]}
+short_final_style=${dictionary[$final_style]}
+
 torchrun --nproc_per_node=8 --rdzv-endpoint localhost:29512 code_trainer.py \
-    --model_name_or_path codellama/CodeLlama-7b-hf \
+    --model_name_or_path codellama/CodeLlama-13b-hf \
     --model_revision 533ac5fc570d52216e713201835b7a3a2af990eb \
-    --refactored_base_path ./code_contests_enumerated_train \
-    --refactored_style base_original \
+    --refactored_base_path TODO \
+    --refactored_style $refactored_style \
+    --final_style $final_style \
     --low_cpu_mem_usage True \
     --use_xformer_attn True \
     --bf16 True \
     --tf32 True \
-    --output_dir checkpoints_codellama_13b_contests_base_4e5_256_1 \
+    --output_dir "checkpoints_codellama_13b_contests_${short_refactored_style}_2e5_256_1" \
     --num_train_epochs 1 \
     --gradient_checkpointing True \
     --gradient_accumulation_steps 32 \
@@ -18,14 +36,14 @@ torchrun --nproc_per_node=8 --rdzv-endpoint localhost:29512 code_trainer.py \
     --save_total_limit 10 \
     --evaluation_strategy "steps" \
     --eval_steps 25 \
-    --learning_rate 4e-5 \
+    --learning_rate 2e-5 \
     --weight_decay 0 \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 5 \
     --block_size 2048 \
     --report_to wandb \
-    --run_name codellama_13b_contests_base_4e5_256_1 \
+    --run_name codellama_13b_contests_base_2e5_256_1 \
     --do_train \
     --do_eval \
     --fsdp "full_shard auto_wrap" \
