49025bd4496bf6909c3d202518b693d2c81a53ce
Naman Jain <naman1205jain@gmail.com>
2023-09-15 23:04:53 -0700

    inference script

--- inference/scripts/code_contests_7b_eval.sh
+++ inference/scripts/code_contests_7b_eval.sh
@@ -2,6 +2,8 @@ difficulty=$1
 model_path=$2
 exp_name=$3
 temperature=$4
+n_samples=200
+batch_size=50
 
 if [ $difficulty == "easy" ]
 then
@@ -20,6 +22,13 @@ else
     exit 1
 fi
 
+if [ $temperature == "0.7" ]
+then
+    n_samples=50
+    batch_size=50
+fi
+
+echo "Running inference for $task with $model_path and temp $temperature n_samples $n_samples batch_size $batch_size"
 
 python main.py \
 --model $model_path --use_auth_token \
