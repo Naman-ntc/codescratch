5a2bce6d4b6fe3fdb8f2a28ffbe3c99f60e2aba4
Naman Jain <naman1205jain@gmail.com>
2023-09-17 16:44:55 -0700

    better scripts

--- inference/scripts/llama_apps_few_shot.sh
+++ inference/scripts/llama_apps_few_shot.sh
@@ -2,4 +2,4 @@ python main.py \
 --model codellama/CodeLlama-7b-hf --use_auth_token \
 --trust_remote_code --tasks appsfewshot-introductory-base --batch_size 10 --n_samples 50 \
 --max_sequence_length 6154 --precision bf16 --temperature 0.7 --max_num_batched_tokens 16000 \
---exp_name clfs_intro_base_07 --allow_code_execution --shuffle --num_gpus 4 \
+--exp_name clfs_apps_intro_07 --allow_code_execution --shuffle --num_gpus 4 \
--- inference/scripts/llama_contests_few_shot.sh
+++ inference/scripts/llama_contests_few_shot.sh
@@ -1,5 +1,5 @@
 python main.py \
 --model codellama/CodeLlama-7b-hf --use_auth_token \
---trust_remote_code --tasks humaneval --batch_size 10 --n_samples 50 \
---max_sequence_length 1024 --precision bf16 --temperature 0.7 --max_num_batched_tokens 16000 \
---exp_name starcoder_humaneval_03 --allow_code_execution --shuffle \
+--trust_remote_code --tasks codecontestsfewshot-easy-base --batch_size 10 --n_samples 50 \
+--max_sequence_length 6154 --precision bf16 --temperature 0.7 --max_num_batched_tokens 16000 \
+--exp_name clfs_contests_easy_base_07 --allow_code_execution --shuffle \
