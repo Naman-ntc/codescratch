46468bb3a4b8ce88d2f66177aafa87184b3f02c5
Naman Jain <naman1205jain@gmail.com>
2023-09-17 16:21:46 -0700

    few shot exp

--- inference/scripts/llama_contests_few_shot.sh
+++ inference/scripts/llama_contests_few_shot.sh
@@ -1,5 +1,5 @@
 python main.py \
 --model codellama/CodeLlama-7b-hf --use_auth_token \
---trust_remote_code --tasks humaneval --batch_size 20 --n_samples 20 \
---max_sequence_length 1024 --precision bf16 --temperature 0.3 --max_num_batched_tokens 16000 \
+--trust_remote_code --tasks humaneval --batch_size 10 --n_samples 50 \
+--max_sequence_length 1024 --precision bf16 --temperature 0.7 --max_num_batched_tokens 16000 \
 --exp_name starcoder_humaneval_03 --allow_code_execution --shuffle \
